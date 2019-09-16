dfc0011b6821b5e4003f4ef0d55ed1b0a850ea87
PiotrSzponar <18piotrek06@gmail.com>
2019-09-16 17:47:46 +0200

    Fix remove temp dir

--- controllers/userController.js
+++ controllers/userController.js
@@ -220,18 +220,16 @@ exports.completeProfile = catchAsync(async (req, res, next) => {
     );
   }
   // Filtered out unwanted fields names that are not allowed to be updated, remove empties
-  const filteredBody =
-    req.route.path === '/me' &&
-    filterObj(
-      req.body,
-      'gender',
-      'birthDate',
-      'bio',
-      'country',
-      'city',
-      'isHidden',
-      'deletePhoto'
-    );
+  const filteredBody = filterObj(
+    req.body,
+    'gender',
+    'birthDate',
+    'bio',
+    'country',
+    'city',
+    'isHidden',
+    'deletePhoto'
+  );
   if (req.body.name) filteredBody.name = req.body.name;
 
   // Mark user profile as completed
